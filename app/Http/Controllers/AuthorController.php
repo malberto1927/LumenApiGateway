86c5b54587e8b6681216968166d77aae042bee6e
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2018-10-29 18:04:34 -0600

    Se han creado los controladores para Authors y Books

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+      // code...
+    }
+
+    public function store(Request $request)
+    {
+      // code...
+    }
+
+    public function show($author)
+    {
+      // code...
+    }
+
+    public function update(Request $request, $author)
+    {
+      // code...
+    }
+
+    public function destroy($author)
+    {
+      // code...
+    }
+}
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+      // code...
+    }
+
+    public function store(Request $request)
+    {
+      // code...
+    }
+
+    public function show($book)
+    {
+      // code...
+    }
+
+    public function update(Request $request, $book)
+    {
+      // code...
+    }
+
+    public function destroy($book)
+    {
+      // code...
+    }
+}
