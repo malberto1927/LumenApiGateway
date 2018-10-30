1127a237a89a6bd392efeb59fed34ea2dac58f74
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2018-10-29 18:39:55 -0600

    Se ha generado la capacidad de consumir servicios externos.

---
+++ app/Traits/ConsumesExternalService.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mario
+ * Date: 29/10/18
+ * Time: 18:31
+ */
+
+namespace App\Traits;
+
+use GuzzleHttp\Client;
+
+trait ConsumesExternalService
+{
+
+    /**
+     * Send a request to any service
+     *
+     * @param $method
+     * @param $requestUrl
+     * @param array $formParams
+     * @param array $headers
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function performRequest($method, $requestUrl, $formParams = [], $headers = [])
+    {
+        $client = new Client([
+            'base_uri' => $this->baseUri,
+        ]);
+
+        $response = $client->request($method, $requestUrl, ['form_params' => $formParams, 'headers' => $headers]);
+
+        return $response->getBody()->getContents();
+    }
+}
---
+++ config/services.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mario
+ * Date: 29/10/18
+ * Time: 18:29
+ */
+
+return [
+
+];
