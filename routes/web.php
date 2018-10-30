99ba2d252009a3d8b6993b0da26789429d7b8089
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2018-10-29 18:25:47 -0600

    Se agregaron las rutas de los servicios

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+/*
+ * Authors routes
+ */
+$router->group(['prefix' => 'authors'], function () use ($router) {
+    $router->get('/', 'AuthorController@index');
+    $router->post('/', 'AuthorController@store');
+    $router->get('/{author}', 'AuthorController@show');
+    $router->put('/{author}', 'AuthorController@update');
+    $router->patch('/{author}', 'AuthorController@update');
+    $router->delete('/{author}', 'AuthorController@destroy');
+});
+
+/*
+ * Books routes
+ */
+$router->group(['prefix' => 'books'], function () use ($router) {
+    $router->get('/', 'BookController@index');
+    $router->post('/', 'BookController@store');
+    $router->get('/{book}', 'BookController@show');
+    $router->put('/{book}', 'BookController@update');
+    $router->patch('/{book}', 'BookController@update');
+    $router->delete('/{book}', 'BookController@destroy');
 });
